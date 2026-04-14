2724176cc0585dc411ebc779e66d996dd6010db6
Pedro-Simoes47 <pedrohms.phms47@gmail.com>
2026-04-14 09:45:41 -0300

    Segurança: Removendo o google-services.json do repositorio

--- kero_pro/keropro/lib/telas/tela_login.dart
+++ kero_pro/keropro/lib/telas/tela_login.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:keropro/telas/tela_cadastro.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class TelaLogin extends StatelessWidget{
+class TelaLogin extends StatefulWidget{
   const TelaLogin({super.key});
+
+  @override
+  State<TelaLogin> createState() => _TelaLoginState();
+}
+
+class _TelaLoginState extends State<TelaLogin> {
+  final _emailController = TextEditingController();
+  final _senhaController = TextEditingController();
+
+  Future<void> _fazerLogin() async {
+    String emailDigitado = _emailController.text;
+    String senhaDigitada = _senhaController.text;
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailDigitado,
+        password: senhaDigitada,
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Login realizado com sucesso! 🚀'),
+          backgroundColor: Colors.green,
+        ),
+      );
+
+      // (No futuro, aqui o app pulará para a Tela Principal do encanador/pintor)
+
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('E-mail ou senha incorretos!'),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,18 +49,21 @@ class TelaLogin extends StatelessWidget{
         child: Column(mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text('KeroPro', style: TextStyle(fontSize: 40, 
+              Text('KeroPro', style: TextStyle(fontSize: 40,
                   fontWeight: FontWeight.bold),
               ),
               SizedBox(height: 40,),
-              TextField(decoration: InputDecoration(labelText: 'E-mail:',
-                  border: OutlineInputBorder()),),
+              TextField(controller: _emailController,
+                decoration: InputDecoration(labelText: 'E-mail',
+                    border: OutlineInputBorder()),
+                keyboardType: TextInputType.emailAddress,),
               SizedBox(height: 24,),
-              TextField(obscureText: true, 
-              decoration: InputDecoration(labelText: 'Senha:',
-              border: OutlineInputBorder()),),
+              TextField(controller: _senhaController,
+                decoration: InputDecoration(labelText: 'Senha',
+                    border: OutlineInputBorder()),
+                obscureText: true,),
               SizedBox(height: 24,),
-              ElevatedButton(onPressed: () {}, child: Text('Entrar')),
+              ElevatedButton(onPressed: _fazerLogin, child: Text('Entrar')),
               TextButton(onPressed: () {Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const TelaCadastro())
